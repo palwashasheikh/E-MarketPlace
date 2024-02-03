a58a00489653a34c420b55205878c5e45003ad8d
palwashasheikh <palwashasheikh229@gmail.com>
2024-02-03 14:03:41 +0400

    new

--- server/database/db.js
+++ server/database/db.js
@@ -4,11 +4,10 @@ const Connection = async (username, password) => {
     // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL = `mongodb+srv://user:Izhaan123@cluster0.uzgbews.mongodb.net/?retryWrites=true&w=majority`
-<<<<<<< HEAD
-    const URL = `mongodb+srv://user:123@cluster0.ilz25mo.mongodb.net/?retryWrites=true&w=majority`
-=======
+    // const URL = `mongodb+srv://user:123@cluster0.ilz25mo.mongodb.net/?retryWrites=true&w=majority`
+
     const URL = `mongodb+srv://user:123@marketplace.t5ndo7t.mongodb.net/?retryWrites=true&w=majority`
->>>>>>> 7a042fb (new changes of db)
+    
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
