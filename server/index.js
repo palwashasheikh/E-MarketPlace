71116781a8aedb3a398d57f559bde2c6dc4a838d
palwashasheikh <palwashasheikh229@gmail.com>
2024-02-05 13:03:05 +0400

    canges happend

--- server/index.js
+++ server/index.js
@@ -12,20 +12,15 @@ import Routes from './routes/route.js';
 dotenv.config();
 const app = express();
 
-
-const PORT = process.env.port || 8000;
-
+const PORT = 8000;
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
 DefaultData();
-
-
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,6 +37,5 @@ paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
 paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
-paytmParams['EMAIL'] = 'palwashakhan916@gmail.com'
+paytmParams['EMAIL'] = 'kunaltyagi@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
-
